314c1149d00a8b56a32bfe096739a3dffc0c4ad6
ehalt <torikussadikk@gmail.com>
2022-01-20 15:23:33 +0600

    array

--- javascripttutorialdotnet/js-arrays.js
+++ javascripttutorialdotnet/js-arrays.js
@@ -34,3 +34,74 @@
 // let mountains = ['Everest','Fuji','Naga parbat'];
 // mountains[2] = 'k2';
 // console.log(mountains);
+
+
+
+//           getting the array size
+
+
+// let flowers = ['sunflower','kavery','belly'];
+// console.log(flowers.length);
+
+
+
+//                    =-=    Basic operations on arrays =-= 
+
+
+// 1. adding an element to the end of an array 
+
+
+// let seas = ['black sea','caribbean sea','north sea','baltic sea'];
+// console.log(seas);
+// seas.push('red sea');
+// console.log(seas);
+
+
+
+// 2. Adding an element to the begining of an array
+
+
+// let seas = ['black sea','caribbean sea','north sea','baltic sea'];
+// console.log(seas);
+// seas.unshift('Red sea');
+// console.log(seas);
+
+
+
+// 3. Removing an element from the end of an array
+
+
+// let seas = ['Black sea','caribbean sea','north sea','baltic sea'];
+// console.log(seas);
+// const lastElement = seas.pop();
+// console.log(seas);
+
+
+
+
+// 4. Removing an element from the beginning of an array
+
+// let seas = ['Black sea','Caribbean sea','north sea','baltic sea'];
+// console.log(seas);
+// const firstElement = seas.shift();
+// console.log(seas);
+
+
+
+// 5. finding an index of an element in the array
+
+// let sea = ['Black sea','Caribbean sea','North sea','baltic sea'];
+// console.log(sea.indexOf('North sea'));
+
+
+
+
+// 6. Check if an value is an array
+
+
+// let arr = ['bondi','khacha','porbe','khose','fokir','lalon'];
+// console.log(Array.isArray(arr));
+
+
+
+
