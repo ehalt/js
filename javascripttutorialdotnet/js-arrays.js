4957704e86ad1c06ef86506fa8900db0bb6d5dfa
ehalt <torikussadikk@gmail.com>
2022-01-20 15:11:34 +0600

    array

--- javascripttutorialdotnet/js-arrays.js
+++ javascripttutorialdotnet/js-arrays.js
@@ -1,1 +1,36 @@
+//           =-= Array =-= 
 
+
+
+// let scores = new Array();
+// console.log(scores);
+
+// let player = Array(10);
+// console.log(player);
+
+
+// let athletes = new Array(3);
+// console.log(athletes);
+
+
+// let colors = ['red','green','blue'];
+// console.log(colors);
+
+
+// let emptyarray = [];
+// console.log(emptyarray);
+
+
+//                 =-= Accessing JavaScript elements =-=
+
+
+// let names = ['shakil babu','fahim morhsed','Torikus sadik'];
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+
+
+
+// let mountains = ['Everest','Fuji','Naga parbat'];
+// mountains[2] = 'k2';
+// console.log(mountains);
