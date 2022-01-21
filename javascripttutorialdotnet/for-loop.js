b117cf108dd11635545034529685132202fe132c
ehalt <torikussadikk@gmail.com>
2022-01-21 23:05:39 +0600

    for loop

---
+++ javascripttutorialdotnet/for-loop.js
@@ -0,0 +1,44 @@
+// javaScript for loop
+
+
+// for (let i = 1; i < 6; i++) {
+//     console.log(i);
+// }
+
+
+// let j = 1;
+// for(; j < 10; j += 2) {
+//     console.log(j);
+// }
+
+
+// for (let j = 1; ; j += 2) {
+//     console.log(j);
+//     if(j > 10) {
+//         break;
+//     }
+// }
+
+
+
+
+// without any expression example 
+
+
+// let j = 1;
+// for(; ;) {
+//     if(j > 10) {
+//         break;
+//     }
+//     console.log(j);
+//     j +=2;
+// }
+
+
+
+// with out body example 
+
+
+let sum = 0;
+for (let i = 0; i <= 9; i++, sum += i);
+console.log(sum);
