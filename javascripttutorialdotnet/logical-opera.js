cec27d731842ea6c074c5fc7aac83a413132b55d
ehalt <torikussadikk@gmail.com>
2022-01-21 14:55:35 +0600

    logical operators

--- javascripttutorialdotnet/logical-opera.js
+++ javascripttutorialdotnet/logical-opera.js
@@ -1,3 +1,90 @@
 // javaScript logical operators 
 
 
+// ! (logical not)
+// || (logical or)
+// && (logical AND)
+
+
+// The logical not operator
+
+
+// let eligible = false,
+// required = true;
+
+// console.log(!eligible);
+// console.log(!required);
+
+
+
+// console.log(!undefined);
+// console.log(!null);
+// console.log(!20);
+// console.log(!0);
+// console.log(!NaN);
+// console.log(!'');
+// console.log(!{});
+// console.log(!false);
+// console.log(!true);
+
+
+
+
+// Double negation (!!)
+
+
+// let counter = 10;
+// console.log(!!counter);
+
+
+// 2. The logical AND Operator (&&)
+
+
+// let a = 12;
+// let b = 13;
+
+// let result = a && b;
+// console.log(result);
+
+
+
+// let eligible = false,
+//     required = true;
+
+// console.log(eligible && required);
+
+
+
+// let eligible = true,
+//     required = true;
+
+// console.log(eligible && required);
+
+
+
+// short - circuit evaluation 
+
+
+// let b = true;
+// let result = b && (1 / 0);
+// console.log(result);
+
+
+
+
+// 3. The logical OR operator (||)
+
+
+// let eligible = true,
+//     required = false;
+
+// console.log(eligible || required);
+
+
+
+// let eligible = false,
+//     required = false;
+
+// console.log(eligible || required);
+
+
