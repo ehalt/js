08ac71cbee03e9561992cf73a697add026be245b
ehalt <torikussadikk@gmail.com>
2022-01-21 23:35:39 +0600

    function

---
+++ javascripttutorialdotnet/function.js
@@ -0,0 +1,32 @@
+// javaScript function 
+
+
+// function say(message) {
+//     console.log(message);
+// }
+// let result = say('Assalamu alaikum');
+// console.log(result);
+
+
+// function add(a, b) {
+//     return a + b;
+// }
+// let sum = add(10, 12);
+// console.log('Sum is : ', sum);
+
+
+
+// function compare (a, b) {
+//     if(a > b) {
+//         return - 1;
+//     }
+//     else if ( a < b) {
+//         return 1;
+//     }
+//     return 0;
+// }
+// let result = compare(12, 10);
+// console.log(result);
+
+
+
