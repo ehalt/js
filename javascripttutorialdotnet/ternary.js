08a7e9ba9e941fed5157b8b74e06714ad3ff704f
ehalt <torikussadikk@gmail.com>
2022-01-21 15:52:34 +0600

    ternary

---
+++ javascripttutorialdotnet/ternary.js
@@ -0,0 +1,10 @@
+// javaScript ternary operator 
+
+
+
+// condition ? expressionIfTrue : expressionIfFalse;
+
+
+let speed = 90;
+let message = speed >= 120 ? 'Too Fast' : 'Fast';
+console.log(message);
