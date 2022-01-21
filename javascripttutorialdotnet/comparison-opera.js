fa575742c8884567d730edf2f3c15daa51a536aa
ehalt <torikussadikk@gmail.com>
2022-01-21 15:24:09 +0600

    comparison operator

---
+++ javascripttutorialdotnet/comparison-opera.js
@@ -0,0 +1,95 @@
+// Comparison operators 
+
+
+// < 
+// >
+// <=
+// >=
+// ==
+// !=
+
+// let r1 = 20 > 10;
+// console.log(r1);
+
+// let r2 = 20 < 10;
+// console.log(r2);
+
+// let r3 = 10 == 10
+// console.log(r3);
+
+
+
+// compare numbers 
+
+// let a = 10,
+//     b = 20;
+
+// console.log(a >= b);
+// console.log(a == 10);
+
+
+
+// compare strings 
+
+
+// let name1 = 'alice',
+//     name2 = 'bob';
+
+// let result = name1 < name2;
+// console.log(result);
+// console.log(name1 == 'alice');
+
+
+
+// compare a number with a value of another type 
+
+
+// console.log(10 < '20');
+
+// console.log(10 == '10');
+
+
+
+
+// comparing object with a non - object 
+
+// let apple = {
+//     valueOf: function() {
+//         return 10;
+//     },
+// };
+
+// let orange = {
+//     toString: function() {
+//         return '20'
+//     },
+// };
+
+// console.log(apple > 10);
+// console.log(orange == 20);
+
+
+
+// console.log(true > 0);
+// console.log(false < 1);
+// console.log(true > false);
+// console.log(false > true);
+// console.log(true >= true);
+// console.log(true <= true);
+// console.log(false <= false);
+// console.log(false >= false);
+
+
+
+// console.log(NaN == 1);
+// console.log(null == undefined);
+// console.log(NaN == NaN);
+
+// console.log(NaN != 1);
+
+
+
+// console.log('10' == 10);
+// console.log('10' === 10);
+
+
