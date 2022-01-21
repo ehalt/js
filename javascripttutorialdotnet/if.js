631c9f14e5704ab817bae8d40d99be9aa3b4ac9e
ehalt <torikussadikk@gmail.com>
2022-01-21 15:29:12 +0600

    javascript if

---
+++ javascripttutorialdotnet/if.js
@@ -0,0 +1,31 @@
+// JavaScript if
+
+
+// let age = 18;
+// if(age >= 18) {
+//     console.log('You can sign up');
+// }
+
+
+// let age = 16;
+// if (age >= 18) {
+//     console.log('You can sign up');
+// }
+
+
+// nested if statement
+
+
+// let age = 16;
+// let state = 'CA';
+
+// if (state == 'CA') {
+//     if (age >= 16) {
+//         console.log('You can drive.');
+//     }
+// }
+
+
+
+
+
