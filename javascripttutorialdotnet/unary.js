2fa3effb275ff1b0aa426961c4c56ea17c995f9f
ehalt <torikussadikk@gmail.com>
2022-01-21 14:34:32 +0600

    unary operators

---
+++ javascripttutorialdotnet/unary.js
@@ -0,0 +1,50 @@
+
+
+//      unary plus 
+
+
+// let x = 10;
+// let y = +x;
+// console.log(y);
+
+
+
+// let s = '10';
+// console.log(typeof(+s));
+
+
+// let f = false;
+// t = true;
+
+// console.log(typeof(+f));
+
+
+// let person = {
+//     name : 'shakil',
+//     toString: function() {
+//         return '22';
+//     },
+// };
+
+// console.log(+person);
+
+
+
+// let age = 25;
+// ++age;
+// console.log(age);
+
+
+
+// let weight = 90;
+// --weight;
+// console.log(weight);
+
+
+
+// let weight = 90;
+// weight = --weight + 5;
+// console.log(weight);
+
+
+
