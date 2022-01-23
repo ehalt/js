fcd17c64110d21804607dd8efea58e7bd74640cd
ehalt <torikussadikk@gmail.com>
2022-01-24 00:22:08 +0600

    anonymous

---
+++ javascripttutorialdotnet/anonymous.js
@@ -0,0 +1,55 @@
+//          =-= Introduction to javaScript anonymous functions =-= 
+
+// an anonymous functon is a function without a name.
+
+
+// let show = function() {
+//     console.log('Anonymous function. :D ');
+// };
+// show();
+
+
+
+// using anonymous functions as arguments 
+
+
+// setTimeout(function(){
+//     console.log('Execute later after 1 second')
+// }, 1000);
+
+
+// immediately invoked function execution
+
+
+// (function(){
+//     console.log('IIFE');
+// })();
+
+
+
+// let person = {
+//     first_name : 'torikus',
+//     last_name : 'sadik',
+// };
+
+// (function() {
+//     console.log(person.fir)
+// })
+
+
+
+
+// arrow function 
+
+
+// let show = function() {
+//     console.log('Anonymous function');
+// }; show();
+
+
+
+// let show = () => console.log('Anonymous function');
+
+
+
+
