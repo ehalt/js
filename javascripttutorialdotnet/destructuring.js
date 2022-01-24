8f4371f1a9c9bc74a454c3b0b2181b8d0c2d4215
ehalt <torikussadikk@gmail.com>
2022-01-24 15:25:31 +0600

    destructuring

---
+++ javascripttutorialdotnet/destructuring.js
@@ -0,0 +1,63 @@
+// destucturing technique
+
+
+// const student = ['Torikus Sadik', 112902, 'computer science'];
+// const name = student[0];
+// console.log(name);
+
+// const roll = student[1];
+// console.log(roll);
+
+// const department = student[2];
+// console.log(department);
+
+
+
+// const student = ['shakil babu', 112906, 'computer science'];
+// const [name, roll, department] = student;
+
+// console.log(name);
+// console.log(roll);
+// console.log(department);
+
+
+
+// const numbers = [1, 2, 3, 4, 5, 6];
+// const [one,,, four,, six] = numbers;
+// console.log(one, four, six);
+
+
+// const numbers = [1,2,3,[4,5,6,[7,8,9]],10];
+// const [one,two,three,[four, five, six,[seven, eight, nine]],ten] = numbers;
+// console.log(four);
+
+
+// const person = {
+//     name: 'shakil babu',
+//     age : 20,
+//     department : 'Computer'
+// }
+
+// const name = person.name;
+// const age = person.age;
+// const department = person.department;
+
+// console.log(name);
+// console.log(age);
+// console.log(department);
+
+
+
+
+// const person = {
+//     name: 'shakil babu',
+//     age : 20,
+//     department : 'Computer'
+// }
+
+// const {name, age, department} = person;
+
+// console.log(name);
+// console.log(age);
+// console.log(department);
+
