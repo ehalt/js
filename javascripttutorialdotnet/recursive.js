4da38f1a2155715ebac272b845a1525764930b57
ehalt <torikussadikk@gmail.com>
2022-01-26 07:13:02 +0600

    recursive

---
+++ javascripttutorialdotnet/recursive.js
@@ -0,0 +1,16 @@
+// javaScript recursive function 
+
+// function countDown(n) {
+//     console.log(n);
+//     if(n >= 1)
+//     countDown(n - 1);
+// }
+// countDown(5);
+
+
+// function countDown(n) {
+//     for(let i = n; i >= 0; i--) {
+//         console.log(i);
+//     }
+// }
+// countDown(5);
