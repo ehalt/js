30a20a404476674d8764d9606f21e815365ea45e
[-] Torikus Sadik [-] <41092966+ehalt@users.noreply.github.com>
2024-09-09 08:46:49 +0600

    .

--- Recap/1_let.js
+++ Recap/1_let.js
@@ -6,3 +6,31 @@ if (true) {
 }
 
 console.log(x);
+
+
+
+// another difference
+
+let x = 10;
+{
+    let x = 2;
+    console.log(`inside the block = ${x}`);
+}
+
+console.log(`outside the block = ${x}`);
+
+
+
+// var vs let 
+
+car_name = "bmw";
+let car_name;
+console.log(car_name);
+
+
+// cannot
+car_name = "volvo";
+let car_name;
+console.log(car_name);
+
+
