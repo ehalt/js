54b573dbb8fadf5698c10bc774713562c21be6eb
[-] Torikus Sadik [-] <41092966+ehalt@users.noreply.github.com>
2024-09-09 10:40:37 +0600

    .

---
+++ Recap/data_types.js
@@ -0,0 +1,14 @@
+// js data types 
+
+/*
+        javaScript has 8 datatypes 
+
+        string
+        number
+        bigint
+        boolean
+        undefined
+        null
+        symbol
+        object
+*/
