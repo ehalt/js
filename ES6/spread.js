b1b9d57d753ee3ce3f17ca8467a50aa896d110e6
0xsadik <torikussadik404@gmail.com>
2024-10-02 10:16:50 +0600

    .

---
+++ ES6/spread.js
@@ -0,0 +1,12 @@
+// spread operator 
+
+const max = Math.max(6, 12, 23, 89);
+// console.log(max);
+
+const numbers = [3, 4, 21, 34, 5, 2, 54];
+// console.log(numbers);
+// console.log(...numbers);
+const arrayMax = Math.max(...numbers);
+// console.log(arrayMax);
+
+
