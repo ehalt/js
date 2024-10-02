226fc7919f83bc444b7f05ca4856745b2f6f69a7
0xsadik <torikussadik404@gmail.com>
2024-10-02 10:24:15 +0600

    ,

---
+++ APIrelated/json.js
@@ -0,0 +1,23 @@
+const user = {id: 1, name: 'gorib amir', job: 'actor'};
+// console.log(user);
+
+// javaScript Object Notation (json)
+// const stringified = JSON.stringify(user);
+// console.log(stringified);
+
+
+
+const shop = {
+    owner : 'Alia',
+    address : {
+        street: 'kochukhet vood er goli',
+        city: 'ranabir',
+        country: "BD"
+    },
+    products: ['laptop', 'mic', 'monitor', 'keyboard'],
+    revenue : 45000,
+    isOpen : true,
+    isNew : false
+};
+
+console.log(shop);
