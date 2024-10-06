4eb2a407744f4e7d71b08ea81a91e212200f5890
0xsadik <torikussadik404@gmail.com>
2024-10-06 16:01:56 +0600

    ,

---
+++ APIrelated/app.js
@@ -0,0 +1,13 @@
+
+function loadData2() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+function loadUsers() {
+    const url = 'https://jsonplaceholder.typicode.com/users';
+    fetch(url) 
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
