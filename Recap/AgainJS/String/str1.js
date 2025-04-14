c08d3b2b440a374a1a9b094aa1a44498e381e874
[-] Torikus Sadik [-] <41092966+ehalt@users.noreply.github.com>
2025-04-14 23:29:34 +0600

    .

---
+++ Recap/AgainJS/String/str1.js
@@ -0,0 +1,48 @@
+// string methods 
+
+
+// concat()
+var a = 'i am '
+var b = 'torikus sadik'
+var c = a.concat(b)
+console.log(c)
+
+
+
+// substr()
+
+let str = "example one"
+console.log(str.substr(5))
+
+
+// charAt()
+
+let s = "torikus"
+console.log(s.charAt(4))
+
+
+
+// startsWith()
+
+let s = "tori kus"
+console.log(s.startsWith("tori"))
+
+
+// ends with 
+
+console.log(s.endsWith("kuss"))
+
+
+
+// trim()
+let s = "        ksjdfk          "
+console.log(s.trim())
+
+
+
+
+// split() 
+let s = "kjskldjlajsdklfjslkdjljsdkljf"
+console.log(s.split(','))
+
+
